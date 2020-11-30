3360266074eb31b5617cb1c6a2e4f769c8c01f8d
Jialong ZHANG <JZhang182@slb.com>
2020-11-30 09:32:35 +0800

    feat: adjust api design

--- ParkingLotApi/DTOs/OrderDTO.cs
+++ ParkingLotApi/DTOs/OrderDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ParkingLotApi.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,7 +7,37 @@ namespace ParkingLotApi.DTOs
 {
     public class OrderDTO
     {
+        public OrderDTO()
+        {
+        }
+
+        public OrderDTO(OrderEntity orderEntity)
+        {
+            ParkingLotName = orderEntity.ParkingLotName;
+            PlateNumber = orderEntity.PlateNumber;
+        }
+
         public string ParkingLotName { get; set; }
         public string PlateNumber { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (!(obj is OrderDTO))
+            {
+                return false;
+            }
+
+            return Equals(this, obj as OrderDTO);
+        }
+
+        public bool Equals(OrderDTO @this, OrderDTO other)
+        {
+            return @this.ParkingLotName == other.ParkingLotName && @this.PlateNumber == other.PlateNumber;
+        }
     }
 }
--- ParkingLotApiTest/ControllerTest/OrdersControllerTest.cs
+++ ParkingLotApiTest/ControllerTest/OrdersControllerTest.cs
@@ -21,5 +21,36 @@ namespace ParkingLotApiTest.ControllerTest
         public OrdersControllerTest(CustomWebApplicationFactory<Startup> factory) : base(factory)
         {
         }
+
+        [Fact]
+        public async Task Should_add_order_when_post()
+        {
+            var client = GetClient();
+            var parkingLotDto = new ParkingLotDTO()
+            {
+                Name = "myLot1",
+                Capacity = 1,
+                Location = " ",
+            };
+            var httpContent = JsonConvert.SerializeObject(parkingLotDto);
+            StringContent content = new StringContent(httpContent, Encoding.UTF8, MediaTypeNames.Application.Json);
+            await client.PostAsync($"/parkinglots", content);
+            var orderDto = new OrderDTO()
+            {
+                ParkingLotName = "myLot1",
+                PlateNumber = "234",
+            };
+            var httpContent2 = JsonConvert.SerializeObject(orderDto);
+            StringContent content2 = new StringContent(httpContent2, Encoding.UTF8, MediaTypeNames.Application.Json);
+            var createResponse = await client.PostAsync($"/orders", content2);
+
+            createResponse.EnsureSuccessStatusCode();
+            var scope = Factory.Services.CreateScope();
+            var scopedServices = scope.ServiceProvider;
+            var context = scopedServices.GetRequiredService<ParkingLotContext>();
+            var firstOrder = await context.Orders.FirstOrDefaultAsync();
+
+            Assert.Equal(orderDto, new OrderDTO(firstOrder));
+        }
     }
 }
